853c21f2e0da6311d986ede3a99ed11128ecda30
mikeJT <jeerasaktarat@gmail.com>
2022-08-23 16:13:07 +0700

    about page updated

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>
+      This web page is created for complete the project 1 in se331 cource.
+    </h2>
   </div>
 </template>
